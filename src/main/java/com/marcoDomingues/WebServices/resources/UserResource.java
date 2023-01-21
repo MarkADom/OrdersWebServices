9652d2eb1ec9fff5edd5d9e5332067f944c64751
Marco Domingues <marco.a.dom78@gmail.com>
2023-01-21 00:54:17 +0000

    user entitie and resource

---
+++ src/main/java/com/marcoDomingues/WebServices/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.marcoDomingues.WebServices.resources;
+
+import com.marcoDomingues.WebServices.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController //responding in /users path.
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L,"Maria", "maria@gmail.com","968538383","y5g67f");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
