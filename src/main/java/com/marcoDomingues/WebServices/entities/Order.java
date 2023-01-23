c765576abe61a4f27238e0d7bac247eed3fc1890
Marco Domingues <marco.a.dom78@gmail.com>
2023-01-23 09:58:21 +0000

    Payment, one-to-one association

--- src/main/java/com/marcoDomingues/WebServices/entities/Order.java
+++ src/main/java/com/marcoDomingues/WebServices/entities/Order.java
@@ -31,6 +31,10 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items =  new HashSet<>();
 
+    //we are mapping the two entities to the same id.
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -76,6 +80,14 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems() {
---
+++ src/main/java/com/marcoDomingues/WebServices/entities/Payment.java
@@ -0,0 +1,67 @@
+package com.marcoDomingues.WebServices.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "payment")
+public class Payment implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant monent;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Instant monent, Order order) {
+        this.id = id;
+        this.monent = monent;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMonent() {
+        return monent;
+    }
+
+    public void setMonent(Instant monent) {
+        this.monent = monent;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Payment payment)) return false;
+        return getId().equals(payment.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
